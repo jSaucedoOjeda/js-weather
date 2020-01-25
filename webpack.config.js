1f0a9d741b7f11b7b52f110e2aef084feda75223
jSaucedo <saucedo.jesus@sonora.edu.mx>
2020-01-25 01:47:40 -0700

    webpack configuration, bootswatch, indexhtml

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/app/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+                    template: './src/index.html'
+        })
+    ]
+        
+    
+};
